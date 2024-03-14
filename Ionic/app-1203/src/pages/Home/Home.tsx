5862564271c8aa343e5cfd53da4a9420d8914a67
Mari2213 <mariannasilvapereira15@gmail.com>
2024-03-14 19:24:06 -0300

    config page tarefas

--- Ionic/app-1203/src/pages/Home/Home.tsx
+++ Ionic/app-1203/src/pages/Home/Home.tsx
@@ -1,24 +1,11 @@
 import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer/ExploreContainer';
 import './Home.css';
+import Tarefas from "../Tarefas/Tarefas";
 
 const Home: React.FC = () => {
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Blank</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen>
-                <IonHeader collapse="condense">
-                    <IonToolbar>
-                        <IonTitle size="large">Blank</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
-                <ExploreContainer/>
-            </IonContent>
-        </IonPage>
+        <Tarefas/>
     );
 };
 
--- Ionic/app-1203/src/pages/Tarefas/Tarefas.tsx
+++ Ionic/app-1203/src/pages/Tarefas/Tarefas.tsx
@@ -1,6 +1,23 @@
+import {IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/react';
+
 const Tarefas = () => {
     return (
         <>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tarefas</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                <h1>Heading 1</h1>
+                <h2>Heading 2</h2>
+                <h3>Heading 3</h3>
+                <h4>Heading 4</h4>
+                <h5>Heading 5</h5>
+                <h6>Heading 6</h6>
+
+                <p>Here's a small text description for the content. Nothing more, nothing less.</p>
+            </IonContent>
         </>
     )
 }
