0a79e7ee18632ab8aee874673907cf78e9f146eb
Mari2213 <mariannasilvapereira15@gmail.com>
2024-03-19 17:12:17 -0300

    concluido page tarefas

--- Ionic/app-1203/src/pages/Home/Home.tsx
+++ Ionic/app-1203/src/pages/Home/Home.tsx
@@ -1,12 +1,8 @@
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer/ExploreContainer';
-import './Home.css';
+import "./Home.css";
 import Tarefas from "../Tarefas/Tarefas";
 
 const Home: React.FC = () => {
-    return (
-        <Tarefas/>
-    );
+  return <Tarefas />;
 };
 
 export default Home;
--- Ionic/app-1203/src/pages/Tarefas/Tarefas.tsx
+++ Ionic/app-1203/src/pages/Tarefas/Tarefas.tsx
@@ -1,25 +1,72 @@
-import {IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/react';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { add } from "ionicons/icons";
+import { useState } from "react";
 
-const Tarefas = () => {
-    return (
-        <>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Tarefas</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                <h1>Heading 1</h1>
-                <h2>Heading 2</h2>
-                <h3>Heading 3</h3>
-                <h4>Heading 4</h4>
-                <h5>Heading 5</h5>
-                <h6>Heading 6</h6>
+const Tarefas: React.FC = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Comprar pão", completed: false },
+    { id: 2, title: "Lavar roupas", completed: false },
+    { id: 3, title: "Limpar casa", completed: true },
+    { id: 4, title: "Estudar para prova", completed: false },
+  ]);
 
-                <p>Here's a small text description for the content. Nothing more, nothing less.</p>
-            </IonContent>
-        </>
-    )
-}
+  const addTask = () => {
+    const newTask = {
+      id: tasks.length + 1,
+      title: "Nova Tarefa",
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const toggleTaskCompletion = (id: number) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === id ? { ...task, completed: !task.completed } : task,
+    );
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Lista de Tarefas</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          {tasks.map((task) => (
+            <IonItem
+              key={task.id}
+              button
+              onClick={() => toggleTaskCompletion(task.id)}
+            >
+              <IonLabel>{task.title}</IonLabel>
+              <IonIcon
+                slot="end"
+                icon={task.completed ? "checkmark" : "ellipsis-hotizontal"}
+              />
+            </IonItem>
+          ))}
+        </IonList>
+        <IonButton expand="block">
+          <IonIcon slot="start" icon={add} />
+          Adicionar tarefa
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default Tarefas;
