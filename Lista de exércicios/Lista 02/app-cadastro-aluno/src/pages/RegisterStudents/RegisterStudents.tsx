9faa5626d960e2b8e36322fd4c90aef6046261f9
Mari2213 <mariannasilvapereira15@gmail.com>
2024-03-24 00:10:30 -0300

    list 02

--- Lista de exércicios/Lista 02/app-cadastro-aluno/src/pages/RegisterStudents/RegisterStudents.tsx
+++ Lista de exércicios/Lista 02/app-cadastro-aluno/src/pages/RegisterStudents/RegisterStudents.tsx
@@ -1,11 +1,13 @@
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
+  IonCheckbox,
   IonContent,
-  IonHeader,
   IonInput,
   IonItem,
   IonLabel,
+  IonList,
   IonPage,
   IonSelect,
   IonSelectOption,
@@ -13,8 +15,30 @@ import {
   IonToggle,
   IonToolbar,
 } from "@ionic/react";
+import React, { useState } from "react";
 
 const RegisterStudents = () => {
+  const [aluno, setAluno] = useState({
+    nome: "",
+    sexo: "",
+    telefone: "",
+    matricula: "",
+    bilingue: false,
+    cursos: [],
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(aluno);
+    setAluno({
+      nome: "",
+      sexo: "",
+      telefone: "",
+      matricula: "",
+      bilingue: false,
+      cursos: [],
+    });
+  };
   return (
     <>
       <IonPage>
@@ -25,20 +49,24 @@ const RegisterStudents = () => {
           <IonTitle>Cadasto de Alunos</IonTitle>
         </IonToolbar>
         <IonContent>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <IonItem>
               <IonInput
+                name="nome"
                 type="text"
                 label="Nome:"
                 placeholder="Digite seu nome"
+                value={aluno.nome}
                 required
               ></IonInput>
             </IonItem>
             <IonItem>
               <IonSelect
+                name="sexo"
                 label="Sexo:"
                 labelPlacement="fixed"
                 placeholder="Selecione o sexo"
+                value={aluno.sexo}
               >
                 <IonSelectOption value="Femenino">Femenino</IonSelectOption>
                 <IonSelectOption value="Maculino">Maculino</IonSelectOption>
@@ -46,23 +74,84 @@ const RegisterStudents = () => {
             </IonItem>
             <IonItem>
               <IonInput
+                name="telefone"
                 type="tel"
                 label="Telefone:"
                 placeholder="Digite o seu telefone"
+                value={aluno.telefone}
                 required
               ></IonInput>
             </IonItem>
             <IonItem>
               <IonInput
+                name="matricula"
                 type="text"
                 label="Matrícula:"
                 placeholder="Digite a sua matrícula"
+                value={aluno.matricula}
                 required
               ></IonInput>
             </IonItem>
             <IonItem>
-              <IonToggle>Aluno Bilingue?</IonToggle>
+              <IonToggle name="bilingue" checked={aluno.bilingue}>
+                Aluno Bilingue?
+              </IonToggle>
             </IonItem>
+            <IonItem>
+              <IonLabel className="ion-text-center">Cursos</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonList>
+                <IonCheckbox
+                  justify="start"
+                  alignment="center"
+                  name="HTML"
+                  value={aluno.cursos}
+                >
+                  HTML
+                </IonCheckbox>
+                <IonCheckbox
+                  justify="start"
+                  alignment="center"
+                  name="CSS"
+                  value={aluno.cursos}
+                >
+                  CSS
+                </IonCheckbox>
+                <IonCheckbox
+                  justify="start"
+                  alignment="center"
+                  name="JavaScript"
+                  value={aluno.cursos}
+                >
+                  JavaScript
+                </IonCheckbox>
+                <IonCheckbox
+                  justify="start"
+                  alignment="center"
+                  name="Java"
+                  value={aluno.cursos}
+                >
+                  Java
+                </IonCheckbox>
+                <IonCheckbox
+                  justify="start"
+                  alignment="center"
+                  name="TypeScript"
+                  value={aluno.cursos}
+                >
+                  TypeScript
+                </IonCheckbox>
+              </IonList>
+            </IonItem>
+            <IonButton
+              type="submit"
+              className="ion-padding"
+              expand="full"
+              shape="round"
+            >
+              Cadastrar
+            </IonButton>
           </form>
         </IonContent>
       </IonPage>
