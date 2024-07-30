58fd5aed322230cc5d97492da340aaf412451c11
Mari2213 <mariannasilvapereira15@gmail.com>
2024-07-29 22:58:36 -0300

    build: create component

---
+++ Lista de exércicios/Lista-03/src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import { IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+
+const Nav = () => {
+  return (
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Page Principal</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  );
+};
+
+export default Nav;
